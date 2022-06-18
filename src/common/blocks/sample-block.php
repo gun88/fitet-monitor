596f3aa1af8c6abc69da9c203f427a0b8b1d186a
tpomante <gun88@hotmail.it>
2022-06-18 19:21:22 +0200

    refactoring

--- src/common/blocks/sample-block.php
+++ src/common/blocks/sample-block.php
@@ -14,44 +14,28 @@
  */
 function sample_block_block_init() {
 	// Skip block registration if Gutenberg is not enabled/merged.
-	if ( ! function_exists( 'register_block_type' ) ) {
+	if (!function_exists('register_block_type')) {
 		return;
 	}
-	$dir = dirname( __FILE__ );
+	$dir = dirname(__FILE__);
 
 	$index_js = 'sample-block/index.js';
-	wp_register_script(
-		'sample-block-block-editor',
-		plugins_url( $index_js, __FILE__ ),
-		[
-			'wp-blocks',
-			'wp-i18n',
-			'wp-element',
-		],
-		filemtime( "{$dir}/{$index_js}" )
+	wp_register_script('sample-block-block-editor', plugins_url($index_js, __FILE__), ['wp-blocks', 'wp-i18n', 'wp-element',], filemtime("{$dir}/{$index_js}")
 	);
 
 	$editor_css = 'sample-block/editor.css';
-	wp_register_style(
-		'sample-block-block-editor',
-		plugins_url( $editor_css, __FILE__ ),
-		[],
-		filemtime( "{$dir}/{$editor_css}" )
+	wp_register_style('sample-block-block-editor', plugins_url($editor_css, __FILE__), [], filemtime("{$dir}/{$editor_css}")
 	);
 
 	$style_css = 'sample-block/style.css';
-	wp_register_style(
-		'sample-block-block',
-		plugins_url( $style_css, __FILE__ ),
-		[],
-		filemtime( "{$dir}/{$style_css}" )
+	wp_register_style('sample-block-block', plugins_url($style_css, __FILE__), [], filemtime("{$dir}/{$style_css}")
 	);
 
-	register_block_type( 'fitet-monitor/sample-block', [
+	register_block_type('fitet-monitor/sample-block', [
 		'editor_script' => 'sample-block-block-editor',
-		'editor_style'  => 'sample-block-block-editor',
-		'style'         => 'sample-block-block',
-	] );
+		'editor_style' => 'sample-block-block-editor',
+		'style' => 'sample-block-block',
+	]);
 }
 
-add_action( 'init', 'sample_block_block_init' );
+add_action('init', 'sample_block_block_init');
