c03de5f2e5e4080ec2ec424beefff8367a928118
tpomante <gun88@hotmail.it>
2022-07-12 13:17:23 +0200

    checkpoint

---
+++ src/public/components/team-standings/class-fitet-monitor-team-standings-component.php
@@ -0,0 +1,93 @@
+<?php
+
+require_once FITET_MONITOR_DIR . 'common/includes/class-fitet-monitor-component.php';
+require_once FITET_MONITOR_DIR . 'public/components/table/class-fitet-monitor-table-component.php';
+
+class Fitet_Monitor_Team_Standings_Component extends Fitet_Monitor_Component {
+	protected function components() {
+		return [
+			'table' => new Fitet_Monitor_Table_Component($this->plugin_name, $this->version),
+			'teamCell' => new Fitet_Monitor_Team_Cell_Component($this->plugin_name, $this->version),
+		];
+	}
+
+
+	protected function process_data($data) {
+		$json_encode = json_encode($data, 128);
+		return [
+			'teamStandingsLabel' => __("Standings"),
+			'table' => $this->table($data),
+			'json' => "<code>$json_encode</code>",
+		];
+	}
+
+	private function table($data) {
+		return $this->components['table']->render(
+			[
+				'name' => 'fm-team-standings',
+				'columns' => $this->columns(),
+				'sort' => $this->sort(),
+				'rows' => $this->rows($data),
+			]
+		);
+	}
+
+	private function columns() {
+
+		return [
+			'ranking' => '#',
+			// "teamId" => __('teamId'),
+			"team" => __('Team'),
+			// "clubCode" => __('clubCode'),
+			// "clubName" => __('clubName'),
+			// "teamStatus" => __('teamStatus'),
+			"points" => __('Points'),
+			"id" => __('ID'),
+			"iv" => __('IV'),
+			"ipa" => __('IPA'),
+			"ip" => __('IP'),
+			"pav" => __('PAV'),
+			"pap" => __('PAP'),
+			"sv" => __('SV'),
+			"sp" => __('SP'),
+			"pv" => __('PV'),
+			"pp" => __('PP'),
+			"pe" => __('PE'),
+
+
+		];
+	}
+
+	private function sort() {
+		return [
+			'ranking' => 'number',
+			"points" => 'number',
+			"id" => 'number',
+			"iv" => 'number',
+			"ipa" => 'number',
+			"ip" => 'number',
+			"pav" => 'number',
+			"pap" => 'number',
+			"sv" => 'number',
+			"sp" => 'number',
+			"pv" => 'number',
+			"pp" => 'number',
+			"pe" => 'number',
+		];
+	}
+
+	private function rows($data) {
+		return array_map(function ($standing) {
+			$standing['team'] = $this->components['teamCell']->render([
+				'clubCode' => $standing['clubCode'],
+				'teamName' => $standing['teamName'],
+				'teamPageUrl' => $standing['teamPageUrl'],
+				'clubLogo' => $standing['clubLogo'],
+			]);
+			unset($standing['teamName']);
+			return $standing;
+		}, $data);
+	}
+
+
+}
